dfeb2fa637abb4e4f0a693c34b9b4f27a113e6a1
TanishJadhav29 <jtanish4629@gmail.com>
2026-09-09 02:42:25 +0530

    Solved: Count Commas in Range (leetcode)

---
+++ leetcode/Easy/count-commas-in-range.cpp
@@ -0,0 +1,14 @@
+// Problem: Count Commas in Range
+// Platform: leetcode
+// Rating/Difficulty: Easy
+// Language: cpp
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/count-commas-in-range/
+// Solved on: 2026-09-08T21:12:24.794Z
+
+class Solution {
+public:
+    int countCommas(int n) {
+        return max(0,n-999);
+    }
+};
